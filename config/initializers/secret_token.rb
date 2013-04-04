520f753a10775f2cd1d095b34f09c35fe78ffb10
vagrant <jeanfbrito@gmail.com>
2013-04-04 02:18:07 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Benzin::Application.config.secret_token = 'd6a82cb8dcbd416178975647ede115bfd42cc4ffb6cad32cc06f96695d41b4a4337666634ea5f04e9682cbf20f75744cf6e52481910b74709dc8344139377e54'
